c61edd59034cc2dfc0c8fbc14f85613ca188abdc
Tobias <tobias@fixmycity.de>
2023-05-01 20:59:12 +0200

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env sh
+
+# Setup:
+#   chmod +x deploy.sh
+
+# abort on errors
+set -e
+
+# Increase version
+# Which will commit the change version number; which is then available at build time
+# Docs https://docs.npmjs.com/cli/v8/commands/npm-version
+npm version patch
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd build
+
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+# copy our app (index.html) as 404.html so our routing works with "open in new tab" (by serving and using the 404 page)
+cp index.html 404.html
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# https://osmberlin.github.io/osm-traffic-sign-tool/
+git push -f git@github.com:osmberlin/osm-traffic-sign-tool.git main:gh-pages
+
+open https://osmberlin.github.io/osm-traffic-sign-tool/
+open https://github.com/osmberlin/osm-traffic-sign-tool/actions
+
+cd -
