c3208247be1f8171cb2ff6a760ccfaf6a3b4ec6a
Tobias <tobias@fixmycity.de>
2023-05-04 06:52:37 +0200

    Data: Update `npm run data:wiki:applyParsedWiki`
    - Create a json that can be used to update `trafficSigns.ts`
    - Don't create a merged file anymore, we will show both data side by side in `/signs/

--- src/data/wiki/parseWiki/applyParsedWiki.ts
+++ src/data/wiki/parseWiki/applyParsedWiki.ts
@@ -1,52 +1,52 @@
+import type { TrafficSigns } from '@/data/types'
 import fs from 'fs'
 import path from 'path'
-import {
-	trafficSigns,
-	type TrafficSignsWithWiki,
-	type TrafficSignWithWiki
-} from '../../trafficSigns'
-import wikiData from './trafficSignsWiki.json'
+import { trafficSigns } from '../../trafficSigns'
+import trafficSignsWiki from './trafficSignsWiki.json'
 
 const svgDirPath = path.join(__dirname, '../../../../static/trafficSignsSvgs')
-const localTrafficSigns = structuredClone(trafficSigns) as TrafficSignsWithWiki
+const localTrafficSigns = structuredClone(trafficSigns) as TrafficSigns
 const matchedWikiDataKeys: string[] = []
-
-// Add wiki data to trafficSigns
-Object.entries(localTrafficSigns).forEach(([key, values]) => {
-	const wikiInfo = wikiData.find(
-		(item) => item.sign === key || item.sign === values.urlString
-	) as TrafficSignWithWiki['wikiData']
-
-	if (wikiInfo) {
-		localTrafficSigns[key].wikiData = wikiInfo
-		matchedWikiDataKeys.push(key)
-	}
-})
 
 // Add local file path to trafficSigns
 fs.readdir(svgDirPath, async (err, files) => {
 	if (err) throw err
 
 	for (const svgFileName of files) {
-		const sign = Object.entries(localTrafficSigns).find(
-			([_, values]) =>
-				values.wikiData && decodeURIComponent(values.wikiData.imageSvg).includes(svgFileName)
+		const wikiSign = trafficSignsWiki.find((values) =>
+			decodeURIComponent(values.imageSvg).includes(svgFileName)
 		)
+		const signKey = Object.entries(localTrafficSigns)
+			.find(([key, values]) => {
+				return wikiSign && (wikiSign.sign === key || wikiSign.sign === values.urlString)
+			})
+			?.at(0)
+		const sign = signKey && localTrafficSigns[signKey as string]
 
-		if (sign) {
+		if (wikiSign && sign) {
 			const file = path.join('/trafficSignsSvgs/', svgFileName)
-			localTrafficSigns[sign[0]].localFile = file
+			sign.image = {
+				svgSourceUrl: wikiSign.imageSvg,
+				svgPath: file,
+				sourceUrl: wikiSign.imageUrl,
+				licence: 'Public Domain'
+			}
+			matchedWikiDataKeys.push(wikiSign.sign)
+			console.log(sign, matchedWikiDataKeys)
 		}
 	}
 
 	await fs.promises.writeFile(
-		path.join(__dirname, '../../trafficSignsWithWiki.json'),
+		path.join(__dirname, 'tempTrafficSignsWithSvg.json'),
 		JSON.stringify(localTrafficSigns, null, 2)
 	)
-})
 
-const unmatchedWikiData = wikiData.filter((e) => !matchedWikiDataKeys.includes(e.sign))
-await fs.promises.writeFile(
-	path.join(__dirname, '../../unmatchedWikiData.json'),
-	JSON.stringify(unmatchedWikiData, null, 2)
-)
+	// This part needs to be inside the `readdir` due to async JS
+	const unmatchedWikiData = trafficSignsWiki.filter(
+		(entry) => !matchedWikiDataKeys.includes(entry.sign)
+	)
+	await fs.promises.writeFile(
+		path.join(__dirname, 'unmatchedWikiData.json'),
+		JSON.stringify(unmatchedWikiData, null, 2)
+	)
+})
