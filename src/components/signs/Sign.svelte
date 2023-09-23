1cdc9facf3bef3a892e04d5a66c25b38f72d246c
Tobias <tobias@fixmycity.de>
2023-09-23 07:45:17 +0200

    Signs: Move debug info into modal

--- src/components/signs/Sign.svelte
+++ src/components/signs/Sign.svelte
@@ -1,19 +1,22 @@
 <script lang="ts">
 	import type { TrafficSign } from '@/data/types'
+	import { IconBug, IconCircleCheck, IconCirclePlus } from '@tabler/icons-svelte'
 	import { twJoin } from 'tailwind-merge'
-	import { Icon, PlusCircle, CheckCircle } from 'svelte-hero-icons'
+	import { buttonStyleSecondary } from '../links/buttonStyles'
 
 	export let active: boolean
 	export let toggleSelection: (signId: string) => undefined
 	export let sign: TrafficSign
+
+	let debugOpen = false
 </script>
 
-<div class="w-20">
+<div class="relative w-20">
 	<button
 		on:click={toggleSelection(sign.urlKey)}
 		class={twJoin(
 			'group/item relative flex h-20 w-full items-center justify-center rounded border border-stone-200 p-2 hover:bg-stone-200',
-			active ? 'bg-stone-200/50 shadow-inner' : ''
+			active ? 'bg-stone-100 shadow' : ''
 		)}
 	>
 		<h3 class="sr-only">
@@ -29,9 +32,9 @@
 			)}
 		>
 			{#if active}
-				<Icon src={CheckCircle} class="h-6 w-6" />
+				<IconCircleCheck class="h-6 w-6" />
 			{:else}
-				<Icon src={PlusCircle} class="h-6 w-6" />
+				<IconCirclePlus class="h-6 w-6" />
 			{/if}
 		</span>
 
@@ -51,8 +54,21 @@
 			</div>
 		{/if}
 	</button>
-	<details class="p-0.5 text-xs text-gray-400">
-		<summary>Debug</summary>
-		<pre class="">{JSON.stringify(sign, undefined, 2)}</pre>
-	</details>
+
+	<dialog open={debugOpen} class="absolute z-10 max-w-md overflow-auto rounded p-5">
+		<form method="dialog" class="absolute right-3 top-3">
+			<button class={buttonStyleSecondary}>&times;</button>
+		</form>
+		<div class="bg-gray-100 py-1 text-center">
+			<p><strong>{sign.urlKey}</strong></p>
+			<p>{sign.descriptiveName}</p>
+		</div>
+		<pre class="mt-5"><code>{JSON.stringify(sign, undefined, 2)}</code></pre>
+	</dialog>
+	<p class="absolute -bottom-2 -right-1">
+		<button
+			class="rounded-full bg-stone-200 p-0.5 text-gray-400 hover:bg-violet-400 hover:text-violet-50"
+			on:click={() => (debugOpen = !debugOpen)}><IconBug class="h-3 w-3" /></button
+		>
+	</p>
 </div>
