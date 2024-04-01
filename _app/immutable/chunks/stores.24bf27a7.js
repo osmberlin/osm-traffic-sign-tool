318b4e5d806ed8cdd205f6582dc9bc044e0b8073
Tobias <tobias@fixmycity.de>
2024-04-01 16:21:47 +0200

    deploy

---
+++ _app/immutable/chunks/stores.24bf27a7.js
@@ -0,0 +1,2 @@
+import{d as e}from"./singletons.6787fc6c.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
+//# sourceMappingURL=stores.24bf27a7.js.map
