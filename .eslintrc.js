be9243e88af0fb64cccb7aa32bd75b674081eb74
Tobias <tobias@fixmycity.de>
2024-07-20 15:08:42 +0200

    Disable `eslint-plugin-react-compiler`
    Which shows errors when active

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,13 @@
 // @ts-check
 /** @type {import("eslint").Linter.Config} */
 const eslintConfig = {
-  // Following https://nextjs.org/docs/app/building-your-application/configuring/eslint
   // https://react.dev/learn/react-compiler#installing-eslint-plugin-react-compiler
-  plugins: ['eslint-plugin-react-compiler'],
-  rules: {
-    'react-compiler/react-compiler': 'error',
-  },
+  // plugins: ['react-compiler'],
+  // rules: {
+  //   'react-compiler/react-compiler': 'error',
+  // },
+
+  // Following https://nextjs.org/docs/app/building-your-application/configuring/eslint
   extends: ['next/core-web-vitals', 'prettier'],
 }
 
