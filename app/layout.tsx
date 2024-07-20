49a98e352ad5d9d6fd4fe2b37f1fdd5a238e9ad8
Tobias <tobias@fixmycity.de>
2024-07-20 16:08:32 +0200

    Migrate tailwind config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
+import { clsx } from 'clsx'
 import type { Metadata } from 'next'
-import { Overpass } from 'next/font/google'
-import './globals.css'
-import { Header } from './_components/layout/Header'
+import { Overpass, Overpass_Mono } from 'next/font/google'
 import { Footer } from './_components/layout/Footer'
+import { Header } from './_components/layout/Header'
 import { TailwindResponsiveHelper } from './_components/layout/TailwindResponsiveHelper'
+import './globals.css'
 
-const inter = Overpass({ subsets: ['latin'] })
+const overpass = Overpass({ subsets: ['latin'], variable: '--font-overpass' })
+const overpassMono = Overpass_Mono({ subsets: ['latin'], variable: '--font-overpass-mono' })
 
 export const metadata: Metadata = {
   title: 'OSM Traffic Sign Tool 2.1',
@@ -17,7 +19,7 @@ type Props = Readonly<{ children: React.ReactNode }>
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={clsx(overpass.variable, overpassMono.variable)}>
         <Header />
         <div className="mx-4 max-w-6xl self-center xl:mx-0">{children}</div>
         <Footer />
