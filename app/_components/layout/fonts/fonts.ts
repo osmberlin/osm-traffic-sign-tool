5b5f274dcabcd7cd8ec21bdab5f5194ef4430c2b
Tobias <tobias@fixmycity.de>
2024-09-14 06:47:04 +0200

    Fonts: Use fontsource for new fonts

---
+++ app/_components/layout/fonts/fonts.ts
@@ -0,0 +1,31 @@
+// FIRA MONO
+//   Preview https://fontsource.org/fonts/fira-sans
+// Variable font supports weights 300-700
+import '@fontsource-variable/fira-code'
+
+// FIRA SANS SERIF
+//   Preview https://fontsource.org/fonts/fira-sans
+// TW FontWeights: https://tailwindcss.com/docs/font-weight
+// `font-thin`       `font-weight: 100`
+// `font-extralight` `font-weight: 200`
+// `font-light`      `font-weight: 300` // THIS
+// `font-normal`     `font-weight: 400` // THIS
+// `font-medium`     `font-weight: 500`
+// `font-semibold`   `font-weight: 600` // THIS
+// `font-bold`       `font-weight: 700` // THIS
+// `font-extrabold`  `font-weight: 800`
+// `font-black`      `font-weight: 900`
+import '@fontsource/fira-sans/300.css'
+import '@fontsource/fira-sans/400.css'
+import '@fontsource/fira-sans/600.css'
+import '@fontsource/fira-sans/700.css'
+
+// FIRA SANS EXTRA CONDENCED
+//   Preview https://fontsource.org/fonts/fira-sans-extra-condensed
+//   Alternative https://fontsource.org/fonts/fira-sans-condensed
+import '@fontsource/fira-sans-extra-condensed'
+
+// NOTO SERIF
+//   Preview https://fontsource.org/fonts/noto-serif
+// Variable font supports weights 100-900
+import '@fontsource-variable/noto-serif'
