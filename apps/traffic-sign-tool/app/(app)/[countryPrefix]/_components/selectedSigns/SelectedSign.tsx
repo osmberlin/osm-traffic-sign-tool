34149b8f093344c8bd710ed557343cc1c1b744d6
Tobias <tobias@fixmycity.de>
2024-10-26 04:15:57 +0200

    Tool: Extract SelectedSignImage Component

--- apps/traffic-sign-tool/app/(app)/[countryPrefix]/_components/selectedSigns/SelectedSign.tsx
+++ apps/traffic-sign-tool/app/(app)/[countryPrefix]/_components/selectedSigns/SelectedSign.tsx
@@ -6,6 +6,7 @@ import { SignStateType } from '@osm-traffic-signs/converter'
 import clsx from 'clsx'
 import { Reorder, useDragControls, useMotionValue } from 'framer-motion'
 import { useState } from 'react'
+import { SelectedSignImage } from './SelectedSignImage'
 import { useRaisedShadow } from './utils/useRaisedShadow'
 
 type Props = {
@@ -75,41 +76,7 @@ export const SelectedSign = ({ sign }: Props) => {
         )}
       >
         <div className="text-center leading-tight">
-          <div className="mx-2 px-3">
-            {'image' in sign && sign.image?.svgPath ? (
-              <div className="relative">
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img
-                  src={sign.image.svgPath}
-                  alt={sign.name}
-                  className="pointer-events-none inline h-auto max-h-24 w-full max-w-24"
-                />
-
-                {sign.signValue &&
-                  sign.valuePrompt &&
-                  sign.signValue !== sign.valuePrompt.defaultValue && (
-                    <div className="absolute inset-0 flex items-center justify-center">
-                      <div className="w-full -rotate-12 rounded bg-amber-100/95 px-3 pt-1 font-condensed text-2xl font-normal text-amber-900 shadow-sm">
-                        {sign.signValue}
-                      </div>
-                    </div>
-                  )}
-              </div>
-            ) : (
-              <div className="flex size-20 items-center justify-center rounded border border-stone-800 bg-stone-600 pt-1 text-stone-50">
-                <code className="whitespace-nowrap tracking-tighter">{sign.signId}</code>
-                <br />
-              </div>
-            )}
-
-            {'name' in sign && sign.name && <h3 className="mt-1 w-full font-light">{sign.name}</h3>}
-
-            {'descriptiveName' in sign && sign.descriptiveName && (
-              <p>
-                <strong className="hyphens-auto break-all">{sign.descriptiveName}</strong>
-              </p>
-            )}
-          </div>
+          <SelectedSignImage sign={sign} />
 
           {'valuePrompt' in sign && sign.valuePrompt && (
             <div className="mx-2 mt-2 rounded border border-stone-400/50 p-1 text-sm leading-tight">
