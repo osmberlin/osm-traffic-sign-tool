8917890bcad6d38f85d65c8be5cc4951d1c6ca07
Tobias <tobias@fixmycity.de>
2024-10-26 06:09:47 +0200

    Data: Add Sign 274.1

--- packages/traffic-sign-converter/src/data/trafficSignData.ts
+++ packages/traffic-sign-converter/src/data/trafficSignData.ts
@@ -760,6 +760,38 @@ export const trafficSignData: SignType[] = [
     },
   },
   {
+    osmValuePart: '274.1[30]',
+    signId: '274.1',
+    name: 'Zeichen 274.1',
+    descriptiveName: 'Tempo 30-Zone',
+    description: '(Anhang)',
+    kind: 'traffic_sign',
+    signValue: 30,
+    valuePrompt: {
+      prompt: 'Geschwindigkeit in km/h ohne Einheit',
+      defaultValue: '30',
+      format: 'integer',
+    },
+    tagRecommendations: {
+      highwayValues: [],
+      uniqueTags: [
+        { key: 'source:maxspeed', value: 'DE:zone' },
+        { key: 'zone:maxspeed', value: 'DE:30' },
+      ],
+      conditionalTag: { key: 'maxspeed', value: '30' },
+    },
+    comments: [],
+    catalogue: {
+      signCategory: 'traffic_sign',
+    },
+    image: {
+      svgPath: '/trafficSignsSvgs/Zeichen_274.1_-_Beginn_einer_Tempo_30-Zone,_StVO_2013.svg',
+      sourceUrl:
+        'https://wiki.openstreetmap.org/wiki/File:Zeichen_274.1_-_Beginn_einer_Tempo_30-Zone,_StVO_2013.svg',
+      licence: 'Public Domain',
+    },
+  },
+  {
     osmValuePart: '274[60]',
     signId: '274',
     name: 'Zeichen 274',
