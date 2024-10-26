2032b019e1a7645687377ee774c39fe3744b42a5
Tobias <tobias@fixmycity.de>
2024-10-26 04:17:03 +0200

    Tool: Footer: Improve external links, add link to project page

--- apps/traffic-sign-tool/app/_components/layout/Footer.tsx
+++ apps/traffic-sign-tool/app/_components/layout/Footer.tsx
@@ -1,11 +1,12 @@
 'use client'
 import { useCountryPrefix } from '@app/app/_store/utils/useCountryPrefix'
 import Link from 'next/link'
+import { ExternalLink } from '../links/ExternalLink'
 
 const navigation = [
   {
     name: 'Imprint & Contact & Privacy Statement (German)',
-    href: 'https://parkraum.osm-verkehrswende.org/contact',
+    href: 'https://www.osm-verkehrswende.org/contact',
   },
   {
     name: 'Code on Github',
@@ -21,27 +22,31 @@ export const Footer = () => {
       <footer>
         <div className="mx-auto max-w-6xl overflow-hidden px-4 py-12 sm:px-6 lg:px-8">
           <p className="mb-8 text-center text-base text-stone-400">
-            This project is part of the OpenStreetMap Verkehrswende Project. <br />
+            This project is part of the{' '}
+            <ExternalLink href="https://www.osm-verkehrswende.org/traffic-signs/">
+              OpenStreetMap Verkehrswende Traffic Sign Project
+            </ExternalLink>
+            . <br />
             It is inspired by the great but unmaintaned{' '}
-            <a
+            <ExternalLink
               className="text-base text-stone-400 underline decoration-stone-700 underline-offset-4 hover:text-stone-400"
               href="http://osmtools.de/traffic_signs/"
-              target="_blank"
+              blank
             >
               osmtools.de/traffic_signs
-            </a>
+            </ExternalLink>
           </p>
           <nav className="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
             {navigation.map((item) => {
               return (
                 <div key={item.href} className="px-5 py-2">
-                  <a
+                  <ExternalLink
                     href={item.href}
                     className="text-base text-stone-400 underline decoration-stone-700 underline-offset-4 hover:text-stone-400"
-                    target="_blank"
+                    blank
                   >
                     {item.name}
-                  </a>
+                  </ExternalLink>
                 </div>
               )
             })}
