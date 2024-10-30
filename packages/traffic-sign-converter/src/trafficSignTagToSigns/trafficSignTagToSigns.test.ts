6b6dcfae2dbbdc91c17c235462491fef736925b1
Tobias <tobias@fixmycity.de>
2024-10-30 04:11:14 +0100

    Package: Improve test helper

--- packages/traffic-sign-converter/src/trafficSignTagToSigns/trafficSignTagToSigns.test.ts
+++ packages/traffic-sign-converter/src/trafficSignTagToSigns/trafficSignTagToSigns.test.ts
@@ -6,6 +6,10 @@ const joinOsmValueParts = (signs: ReturnType<typeof trafficSignTagToSigns>) => {
   return signs.map((s) => s.osmValuePart).join('|')
 }
 
+const joinSignValueParts = (signs: ReturnType<typeof trafficSignTagToSigns>) => {
+  return signs.map((s) => s.signValue).join('|')
+}
+
 describe('trafficSignTagToSigns()', () => {
   const countryPrefix: CountryPrefixType = 'DE'
 
@@ -33,28 +37,28 @@ describe('trafficSignTagToSigns()', () => {
       const result = trafficSignTagToSigns(input, countryPrefix)
       expect(joinOsmValueParts(result)).toMatch('244.1')
       expect(result.map((s) => s.matchdByAlternativeKey).join('|')).toMatch('244')
-      expect(result.map((s) => s.signValue).join('|')).toMatch('')
+      expect(joinSignValueParts(result)).toMatch('')
     })
     test('speed value 60km/h', () => {
       const input = 'traffic_sign=DE:274-60'
       const result = trafficSignTagToSigns(input, countryPrefix)
       expect(joinOsmValueParts(result)).toMatch('274[60]')
       expect(result.map((s) => s.matchdByAlternativeKey).join('|')).toMatch('274-60')
-      expect(result.map((s) => s.signValue).join('|')).toMatch('60')
+      expect(joinSignValueParts(result)).toMatch('60')
     })
     test('speed value 100km/h', () => {
       const input = 'traffic_sign=DE:274-100'
       const result = trafficSignTagToSigns(input, countryPrefix)
       expect(joinOsmValueParts(result)).toMatch('274[100]')
       expect(result.map((s) => s.matchdByAlternativeKey).join('|')).toMatch('274-100')
-      expect(result.map((s) => s.signValue).join('|')).toMatch('100')
+      expect(joinSignValueParts(result)).toMatch('100')
     })
     test('speed value 50km/h', () => {
       const input = 'traffic_sign=DE:274-50'
       const result = trafficSignTagToSigns(input, countryPrefix)
       expect(joinOsmValueParts(result)).toMatch('274[50]')
       expect(result.map((s) => s.matchdByAlternativeKey).join('|')).toMatch('274-50')
-      expect(result.map((s) => s.signValue).join('|')).toMatch('50')
+      expect(joinSignValueParts(result)).toMatch('50')
     })
   })
 
@@ -64,7 +68,7 @@ describe('trafficSignTagToSigns()', () => {
       const input = 'traffic_sign=DE:1040-31[08-11,16-18]'
       const result = trafficSignTagToSigns(input, countryPrefix)
       expect(joinOsmValueParts(result)).toMatch('1040-31')
-      expect(result.map((s) => s.signValue).join('|')).toMatch('08-11,16-18')
+      expect(joinSignValueParts(result)).toMatch('08-11,16-18')
     })
   })
 
@@ -73,7 +77,7 @@ describe('trafficSignTagToSigns()', () => {
       const input = 'traffic_sign=DE:1042-35[Mo,Tue 13-14]'
       const result = trafficSignTagToSigns(input, countryPrefix)
       expect(joinOsmValueParts(result)).toMatch('1042-35')
-      expect(result.map((s) => s.signValue).join('|')).toMatch('Mo,Tue 13-14')
+      expect(joinSignValueParts(result)).toMatch('Mo,Tue 13-14')
     })
   })
 
@@ -82,7 +86,7 @@ describe('trafficSignTagToSigns()', () => {
       const input = 'traffic_sign=DE:260,1026-36;262[4]'
       const result = trafficSignTagToSigns(input, countryPrefix)
       expect(joinOsmValueParts(result)).toMatch('260|1026-36|262[4]')
-      expect(result.map((s) => s.signValue).join('|')).toMatch('4')
+      expect(joinSignValueParts(result)).toMatch('4')
     })
   })
 
@@ -91,9 +95,11 @@ describe('trafficSignTagToSigns()', () => {
       const input = 'traffic_sign=FOO;DE:260,BAR,1026-36;BAZ;262[4];FOO2'
       const result = trafficSignTagToSigns(input, countryPrefix)
       expect(joinOsmValueParts(result)).toMatch('FOO|260|BAR|1026-36|BAZ|262[4]|FOO2')
-      expect(result.map((s) => s.signValue).join('|')).toMatch('FOO||BAR||BAZ|4|FOO2')
+      expect(joinSignValueParts(result)).toMatch('FOO||BAR||BAZ|4|FOO2')
       // TODO: Understand why this is green as well…
-      // expect(result.map((s) => s.signValue).join('|')).toMatch('4')
+      // expect(joinSignValueParts(result)).toMatch('4')
+    })
+  })
     })
   })
 })
