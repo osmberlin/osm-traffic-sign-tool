2e9ba82389f1c6211dcabe24ec5f2ab35b4ae4cd
Tobias <tobias@fixmycity.de>
2024-10-30 06:30:30 +0100

    Tool: Remove `redirects` from next.config to avoid build warning

--- apps/traffic-sign-tool/next.config.mjs
+++ apps/traffic-sign-tool/next.config.mjs
@@ -5,18 +5,10 @@ const nextConfig = {
   experimental: {
     typedRoutes: true,
   },
+  // NOTE: `redirects` don't work with `output: 'export'`
   // Docs: https://nextjs.org/docs/app/building-your-application/routing/redirecting#redirects-in-nextconfigjs
   // Docs: https://nextjs.org/docs/app/api-reference/next-config-js/redirects
-  redirects: async () => [
-    // TODO: Make use only non-www-domains are used; configure once a domain is known
-    //   // https://stackoverflow.com/a/70184067
-    //   {
-    //     source: '/:path*',
-    //     has: [{ type: 'host', value: 'www.DOMAIN.de' }],
-    //     destination: 'https://DOMAIN.de/:path*',
-    //     permanent: true,
-    //   },
-  ],
+
   // Deploy to Github Pages
   // Docs: https://github.com/gregrickaby/nextjs-github-pages?tab=readme-ov-file#nextjs-config
   output: 'export',
