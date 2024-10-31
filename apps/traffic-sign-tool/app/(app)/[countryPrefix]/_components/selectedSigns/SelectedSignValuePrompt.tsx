2220caded2466fa5648737f9d24f814c43a705a4
Tobias <tobias@fixmycity.de>
2024-10-31 04:58:34 +0100

    Tool: Fix custom values forms
    Don't like this solution, but it works for now.

--- apps/traffic-sign-tool/app/(app)/[countryPrefix]/_components/selectedSigns/SelectedSignValuePrompt.tsx
+++ apps/traffic-sign-tool/app/(app)/[countryPrefix]/_components/selectedSigns/SelectedSignValuePrompt.tsx
@@ -1,5 +1,6 @@
 import { useParamSigns } from '@app/app/_store/useParamSigns.nuqs'
 import { SignStateType } from '@osm-traffic-signs/converter'
+import { useEffect, useState } from 'react'
 
 type Props = {
   sign: SignStateType
@@ -14,22 +15,60 @@ const inputFormats = {
 
 export const SelectedSignValuePrompt = ({ sign }: Props) => {
   const { updateSignValue } = useParamSigns()
+  const [internalValue, setInternalValue] = useState<null | number | string>(null)
+  const [typingTimeout, setTypingTimeout] = useState<NodeJS.Timeout | null>(null)
+
+  const value =
+    internalValue === null
+      ? sign.signValue ||
+        ('valuePrompt' in sign && sign.valuePrompt ? sign.valuePrompt.defaultValue : undefined)
+      : internalValue
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInternalValue(event.target.value)
+
+    if (typingTimeout) {
+      clearTimeout(typingTimeout)
+    }
+
+    const timeout = setTimeout(() => {
+      updateSignValue(sign.osmValuePart, event.target.value)
+    }, 2000)
+
+    setTypingTimeout(timeout)
+  }
+
+  const handleBlur = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (typingTimeout) {
+      clearTimeout(typingTimeout)
+    }
+    updateSignValue(sign.osmValuePart, event.target.value)
+  }
+
+  useEffect(() => {
+    return () => {
+      if (typingTimeout) {
+        clearTimeout(typingTimeout)
+      }
+    }
+  }, [typingTimeout])
 
   if (!('valuePrompt' in sign && sign.valuePrompt)) return null
 
   return (
-    <div className="mx-2 mt-2 rounded border border-stone-400/50 p-1 text-sm leading-tight">
+    <form className="mx-2 mt-2 rounded border border-stone-400/50 p-1 text-sm leading-tight">
       <label htmlFor={sign.osmValuePart} className="break-word">
         {sign.valuePrompt.prompt}:
       </label>
       <input
-        onChange={(event) => updateSignValue(sign.osmValuePart, event.target.value)}
+        onChange={handleChange}
+        onBlur={handleBlur}
         name={sign.osmValuePart}
         type={inputFormats[sign.valuePrompt.format].type ?? 'text'}
         step={inputFormats[sign.valuePrompt.format].steps ?? undefined}
-        value={sign.signValue ?? sign.valuePrompt.defaultValue}
+        value={value}
         className="block w-full rounded-md border-0 px-1 py-1.5 text-center text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:leading-6"
       />
-    </div>
+    </form>
   )
 }
