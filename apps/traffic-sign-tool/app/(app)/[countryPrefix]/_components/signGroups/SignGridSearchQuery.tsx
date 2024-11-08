0293385ef42ae8bee8b3b88433a51ea9c91dbbd5
Tobias <tobias@fixmycity.de>
2024-11-08 05:38:01 +0100

    Tool: Fix search
    Closes https://github.com/osmberlin/osm-traffic-sign-tool/issues/62

--- apps/traffic-sign-tool/app/(app)/[countryPrefix]/_components/signGroups/SignGridSearchQuery.tsx
+++ apps/traffic-sign-tool/app/(app)/[countryPrefix]/_components/signGroups/SignGridSearchQuery.tsx
@@ -1,26 +1,25 @@
 'use client'
 import { useParamQ } from '@app/app/_store/useParamQ.nuqs'
 import { useParamQCountActions } from '@app/app/_store/useParamQCount.zustand'
-import { useParamSigns } from '@app/app/_store/useParamSigns.nuqs'
+import { trafficSignData } from '@osm-traffic-signs/converter'
 import { useEffect, useMemo } from 'react'
 import { SignGrid } from './SignGrid'
 
 export const SignGridSearchQuery = () => {
-  const { paramSigns } = useParamSigns()
   const { paramQ } = useParamQ()
   const { setParamQCount } = useParamQCountActions()
 
   const searchSigns = useMemo(() => {
-    return paramSigns.filter((sign) => {
+    const result = trafficSignData.filter((sign) => {
       if (!paramQ) return true
-      if (sign.recodgnizedSign === false) return true
       return (
         sign.osmValuePart.toLocaleLowerCase().includes(paramQ) ||
         sign.descriptiveName?.toLocaleLowerCase()?.includes(paramQ) ||
         sign.description?.toLocaleLowerCase()?.includes(paramQ)
       )
     })
-  }, [paramQ, paramSigns])
+    return result
+  }, [paramQ])
 
   useEffect(() => {
     setParamQCount(searchSigns.length)
