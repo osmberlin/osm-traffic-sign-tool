39fd629a1bc3d4011ba231be4770cb7718ab0b9c
Tobias <tobias@fixmycity.de>
2025-02-21 11:15:52 +0100

    Signs: Add a few of the "exception_modifier" signs

--- packages/traffic-sign-converter/src/data-definitions/DE/trafficSignDataDE.ts
+++ packages/traffic-sign-converter/src/data-definitions/DE/trafficSignDataDE.ts
@@ -1135,6 +1135,27 @@ export const trafficSignDataDE: SignType[] = [
     },
   },
   {
+    osmValuePart: '1022-13',
+    signId: '1022-13',
+    name: 'Zusatzzeichen 1022-13',
+    descriptiveName: 'E-Bikes frei',
+    description: null,
+    kind: 'exception_modifier',
+    tagRecommendations: {
+      highwayValues: [],
+      accessTags: [{ key: 'electric_bicycle', value: 'yes' }],
+    },
+    comments: [],
+    catalogue: {
+      signCategory: 'exception_modifier',
+    },
+    image: {
+      sourceUrl:
+        'https://de.wikipedia.org/wiki/Datei:Zusatzzeichen_1022-13_-_E-Bikes_frei_(450x600),_StVO_2017.svg',
+      licence: 'Public Domain',
+    },
+  },
+  {
     osmValuePart: '1024-10,1022-12',
     signId: '1024-10,1022-12',
     name: 'Zusatzzeichen 1024-10+1022-12',
@@ -1217,7 +1238,7 @@ export const trafficSignDataDE: SignType[] = [
     osmValuePart: '1024-12',
     signId: '1024-12',
     name: 'Zusatzzeichen 1024-12',
-    descriptiveName: 'Kraftfahrzeuge mit einem zulässigen Gesamtgewicht über 3,5 t…',
+    descriptiveName: 'Kraftfahrzeuge mit einem zulässigen Gesamtgewicht über 3,5 t… frei',
     description:
       'Kraftfahrzeuge mit einem zulässigen Gesamtgewicht über 3,5 t, einschließlich ihrer Anhäger und Zugmaschinen, ausgenommen Personenkraftwagen und Kraftomnibusse frei (zulässiges Gewicht, nicht das tatsächliche Gewicht)',
     kind: 'exception_modifier',
@@ -1402,6 +1423,27 @@ export const trafficSignDataDE: SignType[] = [
     },
   },
   {
+    osmValuePart: '1020-32',
+    signId: '1020-32',
+    name: 'Zusatzzeichen 1020-32',
+    descriptiveName: 'Bewohner mit Parkausweis Nr. … frei',
+    description: null,
+    kind: 'exception_modifier',
+    tagRecommendations: {
+      highwayValues: [],
+      uniqueTags: [],
+    },
+    comments: [],
+    catalogue: {
+      signCategory: 'exception_modifier',
+    },
+    image: {
+      sourceUrl:
+        'https://de.wikipedia.org/wiki/Datei:Zusatzzeichen_1020-32_-_Bewohner_mit_Parkausweis_Nr._..._frei,_StVO_2002.svg',
+      licence: 'Public Domain',
+    },
+  },
+  {
     osmValuePart: '1026-33',
     signId: '1026-33',
     name: 'Zusatzzeichen 1026-33',
@@ -1475,6 +1517,322 @@ export const trafficSignDataDE: SignType[] = [
     image: {
       sourceUrl:
         'https://wiki.openstreetmap.org/wiki/File:Zusatzzeichen_1026-38_-_Land-_und_forstwirtschaftlicher_Verkehr_frei_(450x600),_StVO_1992.svg',
+      licence: 'Public Domain',
+    },
+  },
+  {
+    osmValuePart: '1010-50',
+    signId: '1010-50',
+    name: 'Zusatzzeichen 1010-50',
+    descriptiveName: 'Kraftwagen und sonstige mehrspurige Fahrzeuge',
+    description: null,
+    kind: 'condition_modifier',
+    tagRecommendations: {
+      modifierValue: 'motorcar',
+      uniqueTags: [],
+    },
+    comments: [],
+    catalogue: {
+      signCategory: 'condition_modifier',
+    },
+    image: {
+      sourceUrl:
+        'https://de.wikipedia.org/wiki/Datei:Zusatzzeichen_1010-50_-_Kraftwagen_und_sonstige_mehrspurige_Fahrzeuge_(600x330),_StVO_2017.svg',
+      licence: 'Public Domain',
+    },
+  },
+  {
+    osmValuePart: '1010-51',
+    signId: '1010-51',
+    name: 'Zusatzzeichen 1010-51',
+    descriptiveName: 'Kraftfahrzeuge mit einem zulässigen Gesamtgewicht über 3,5 t…',
+    description:
+      'Kraftfahrzeuge mit einer zulässigen Gesamtmasse über 3,5 t, einschließlich ihrer Anhänger, und Zugmaschinen, ausgenommen Personenkraftwagen und Kraftomnibusse',
+    kind: 'condition_modifier',
+    tagRecommendations: {
+      modifierValue: 'hgv',
+      uniqueTags: [],
+    },
+    comments: [],
+    catalogue: {
+      signCategory: 'condition_modifier',
+    },
+    image: {
+      sourceUrl:
+        'https://de.wikipedia.org/wiki/Datei:Zusatzzeichen_1010-51_-_Kraftfahrzeuge_mit_einer_zul%C3%A4ssigen_Gesamtmasse_%C3%BCber_3,5_t_einschlie%C3%9Flich_ihrer_Anh%C3%A4nger_und_Zugmaschinen,_ausgenommen_Personenkraftwagen_und_Kraftomnibusse,_StVO_2017.svg',
+      licence: 'Public Domain',
+    },
+  },
+  {
+    osmValuePart: '1010-52',
+    signId: '1010-52',
+    name: 'Zusatzzeichen 1010-52',
+    descriptiveName: 'Radverkehr',
+    description: null,
+    kind: 'condition_modifier',
+    tagRecommendations: {
+      modifierValue: 'bicycle',
+      uniqueTags: [],
+    },
+    comments: [],
+    catalogue: {
+      signCategory: 'condition_modifier',
+    },
+    image: {
+      sourceUrl:
+        'https://de.wikipedia.org/wiki/Datei:Zusatzzeichen_1010-52_-_Radverkehr_(600x330),_StVO_2017.svg',
+      licence: 'Public Domain',
+    },
+  },
+  {
+    osmValuePart: '1010-53',
+    signId: '1010-53',
+    name: 'Zusatzzeichen 1010-53',
+    descriptiveName: 'Fußgänger',
+    description: null,
+    kind: 'condition_modifier',
+    tagRecommendations: {
+      modifierValue: 'foot',
+      uniqueTags: [],
+    },
+    comments: [],
+    catalogue: {
+      signCategory: 'condition_modifier',
+    },
+    image: {
+      sourceUrl:
+        'https://de.wikipedia.org/wiki/Datei:Zusatzzeichen_1010-53_-_Fu%C3%9Fg%C3%A4nger_(600x330),_StVO_2017.svg',
+      licence: 'Public Domain',
+    },
+  },
+  {
+    osmValuePart: '1010-54',
+    signId: '1010-54',
+    name: 'Zusatzzeichen 1010-54',
+    descriptiveName: 'Reiter',
+    description: null,
+    kind: 'condition_modifier',
+    tagRecommendations: {
+      modifierValue: 'horse',
+      uniqueTags: [],
+    },
+    comments: [],
+    catalogue: {
+      signCategory: 'condition_modifier',
+    },
+    image: {
+      sourceUrl:
+        'https://de.wikipedia.org/wiki/Datei:Zusatzzeichen_1010-54_-_Reiter_(600x330),_StVO_2017.svg',
+      licence: 'Public Domain',
+    },
+  },
+  {
+    osmValuePart: '1010-56',
+    signId: '1010-56',
+    name: 'Zusatzzeichen 1010-56',
+    descriptiveName: 'Straßenbahn',
+    description: null,
+    kind: 'condition_modifier',
+    tagRecommendations: {
+      modifierValue: 'tram',
+      uniqueTags: [],
+    },
+    comments: [],
+    catalogue: {
+      signCategory: 'condition_modifier',
+    },
+    image: {
+      sourceUrl:
+        'https://de.wikipedia.org/wiki/Datei:Zusatzzeichen_1010-56_-_Stra%C3%9Fenbahn,_StVO_2017.svg',
+      licence: 'Public Domain',
+    },
+  },
+  {
+    osmValuePart: '1010-57',
+    signId: '1010-57',
+    name: 'Zusatzzeichen 1010-57',
+    descriptiveName: 'Kraftomnibus',
+    description: null,
+    kind: 'condition_modifier',
+    tagRecommendations: {
+      modifierValue: 'bus',
+      uniqueTags: [],
+    },
+    comments: [],
+    catalogue: {
+      signCategory: 'condition_modifier',
+    },
+    image: {
+      sourceUrl:
+        'https://de.wikipedia.org/wiki/Datei:Zusatzzeichen_1010-57_-_Kraftomnibus,_StVO_2017.svg',
+      licence: 'Public Domain',
+    },
+  },
+  {
+    osmValuePart: '1010-58',
+    signId: '1010-58',
+    name: 'Zusatzzeichen 1010-58',
+    descriptiveName: 'Personenkraftwagen',
+    description: null,
+    kind: 'condition_modifier',
+    tagRecommendations: {
+      modifierValue: 'motorcar',
+      uniqueTags: [],
+    },
+    comments: [],
+    catalogue: {
+      signCategory: 'condition_modifier',
+    },
+    image: {
+      sourceUrl:
+        'https://de.wikipedia.org/wiki/Datei:Zusatzzeichen_1010-58_-_Personenkraftwagen_(600x330),_StVO_2017.svg',
+      licence: 'Public Domain',
+    },
+  },
+  {
+    osmValuePart: '1010-61',
+    signId: '1010-61',
+    name: 'Zusatzzeichen 1010-61',
+    descriptiveName: 'Kraftfahrzeuge bis 25 km/h',
+    description:
+      'Kraftfahrzeuge und Züge, die nicht schneller als 25 km/h fahren können oder dürfen',
+    kind: 'condition_modifier',
+    tagRecommendations: {
+      modifierValue: 'agricultural',
+      uniqueTags: [],
+    },
+    comments: [],
+    catalogue: {
+      signCategory: 'condition_modifier',
+    },
+    image: {
+      sourceUrl:
+        'https://de.wikipedia.org/wiki/Datei:Zusatzzeichen_1010-61_-_Kraftfahrzeuge_und_Z%C3%BCge,_die_nicht_schneller_als_25_km-h_fahren_k%C3%B6nnen_oder_d%C3%BCrfen_(600x330),_StVO_2017.svg',
+      licence: 'Public Domain',
+    },
+  },
+  {
+    osmValuePart: '1010-62',
+    signId: '1010-62',
+    name: 'Zusatzzeichen 1010-62',
+    descriptiveName: 'Krafträder,…',
+    description: 'Krafträder, auch mit Beiwagen, Kleinkrafträder und Mopeds',
+    kind: 'condition_modifier',
+    tagRecommendations: {
+      modifierValue: 'motorcycle',
+      uniqueTags: [],
+    },
+    comments: [],
+    catalogue: {
+      signCategory: 'condition_modifier',
+    },
+    image: {
+      sourceUrl:
+        'https://de.wikipedia.org/wiki/Datei:Zusatzzeichen_1010-62_-_Kraftr%C3%A4der,_auch_mit_Beiwagen,_Kleinkraftr%C3%A4der_und_Mofas_(600x330),_StVO_2017.svg',
+      licence: 'Public Domain',
+    },
+  },
+  {
+    osmValuePart: '1010-63',
+    signId: '1010-63',
+    name: 'Zusatzzeichen 1010-63',
+    descriptiveName: 'Mofas',
+    description: null,
+    kind: 'condition_modifier',
+    tagRecommendations: {
+      modifierValue: 'mofa',
+      uniqueTags: [],
+    },
+    comments: [],
+    catalogue: {
+      signCategory: 'condition_modifier',
+    },
+    image: {
+      sourceUrl:
+        'https://de.wikipedia.org/wiki/Datei:Zusatzzeichen_1010-63_-_Mofas_(600x330),_StVO_2017.svg',
+      licence: 'Public Domain',
+    },
+  },
+  {
+    osmValuePart: '1010-65',
+    signId: '1010-65',
+    name: 'Zusatzzeichen 1010-65',
+    descriptiveName: 'E-Bikes',
+    description: null,
+    kind: 'condition_modifier',
+    tagRecommendations: {
+      modifierValue: 'electric_bicycle',
+      uniqueTags: [],
+    },
+    comments: [],
+    catalogue: {
+      signCategory: 'condition_modifier',
+    },
+    image: {
+      sourceUrl:
+        'https://de.wikipedia.org/wiki/Datei:Zusatzzeichen_1010-65_-_E-Bikes,_StVO_2017.svg',
+      licence: 'Public Domain',
+    },
+  },
+  {
+    osmValuePart: '1010-66',
+    signId: '1010-66',
+    name: 'Zusatzzeichen 1010-66',
+    descriptiveName: 'elektrisch betriebene Fahrzeuge',
+    description: null,
+    kind: 'condition_modifier',
+    tagRecommendations: {
+      uniqueTags: [],
+    },
+    comments: [],
+    catalogue: {
+      signCategory: 'condition_modifier',
+    },
+    image: {
+      sourceUrl:
+        'https://de.wikipedia.org/wiki/Datei:Zusatzzeichen_1010-66_-_elektrisch_betriebene_Fahrzeuge,_StVO_2015.svg',
+      licence: 'Public Domain',
+    },
+  },
+  {
+    osmValuePart: '1010-68',
+    signId: '1010-68',
+    name: 'Zusatzzeichen 1010-68',
+    descriptiveName: 'Elektrokleinstfahrzeuge',
+    description: 'Elektrokleinstfahrzeuge im Sinne der Elektrokleinstfahrzeuge-Verordnung (eKFV)',
+    kind: 'condition_modifier',
+    tagRecommendations: {
+      modifierValue: 'small_electric_vehicle',
+      uniqueTags: [],
+    },
+    comments: [],
+    catalogue: {
+      signCategory: 'condition_modifier',
+    },
+    image: {
+      sourceUrl:
+        'https://de.wikipedia.org/wiki/Datei:Zusatzzeichen_1010-68_-_Elektrokleinstfahrzeuge_im_Sinne_der_Elektrokleinstfahrzeuge-Verordnung_(eKFV),_(450x600),_StVO_2020.svg',
+      licence: 'Public Domain',
+    },
+  },
+  {
+    osmValuePart: '1010-69',
+    signId: '1010-69',
+    name: 'Zusatzzeichen 1010-69',
+    descriptiveName: 'Lastenfahrrad',
+    description: 'Fahrrad zum Transport von Gütern oder Personen',
+    kind: 'condition_modifier',
+    tagRecommendations: {
+      modifierValue: 'cargo_bike',
+      uniqueTags: [],
+    },
+    comments: [],
+    catalogue: {
+      signCategory: 'condition_modifier',
+    },
+    image: {
+      sourceUrl:
+        'https://de.wikipedia.org/wiki/Datei:Zusatzzeichen_1010-69_-_Fahrrad_zum_Transport_von_G%C3%BCtern_oder_Personen_-_Lastenfahrrad_(330x600),_StVO_2020.svg',
       licence: 'Public Domain',
     },
   },
