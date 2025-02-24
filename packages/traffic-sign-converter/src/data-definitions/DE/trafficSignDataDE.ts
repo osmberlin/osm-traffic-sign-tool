0a060847507c44686129bcd4efc9b3430ea0724c
Tobias <tobias@fixmycity.de>
2025-02-24 06:21:04 +0100

    Signs: Add weight signs

--- packages/traffic-sign-converter/src/data-definitions/DE/trafficSignDataDE.ts
+++ packages/traffic-sign-converter/src/data-definitions/DE/trafficSignDataDE.ts
@@ -417,9 +417,9 @@ export const trafficSignDataDE: SignType[] = [
     osmValuePart: '253',
     signId: '253',
     name: 'Zeichen 253',
-    descriptiveName:
+    descriptiveName: 'Verbot für Kraftfahrzeuge mit einem zulässigen Gesamtgewicht über 3,5 t…',
+    description:
       'Verbot für Kraftfahrzeuge mit einem zulässigen Gesamtgewicht über 3,5 t, einschließlich ihrer Anhäger, und Zugmaschinen, ausgenommen Personenkraftwagen und Kraftomnibusse',
-    description: null,
     kind: 'traffic_sign',
     tagRecommendations: {
       highwayValues: [],
@@ -1090,7 +1090,6 @@ export const trafficSignDataDE: SignType[] = [
       licence: 'Public Domain',
     },
   },
-
   {
     osmValuePart: '1022-10',
     signId: '1022-10',
@@ -2093,7 +2092,6 @@ export const trafficSignDataDE: SignType[] = [
     description: null,
     kind: 'condition_modifier',
     tagRecommendations: {
-      highwayValues: [],
       uniqueTags: [],
       modifierValue: 'wet',
     },
@@ -2104,6 +2102,76 @@ export const trafficSignDataDE: SignType[] = [
     image: {
       sourceUrl:
         'https://de.wikipedia.org/wiki/Datei:Zusatzzeichen_1053-35_-_Bei_N%C3%A4sse_(600x600),_StVO_2017.svg',
+      licence: 'Public Domain',
+    },
+  },
+  {
+    osmValuePart: '1053-33',
+    signId: '1053-33',
+    name: 'Zusatzzeichen 1053-33',
+    descriptiveName: 'Massenangabe 7,5 t',
+    description:
+      '7,5 t zulässige Gesamtmasse einschließlich Anhänger überschreitet die angegebene Grenze',
+    kind: 'condition_modifier',
+    tagRecommendations: {
+      uniqueTags: [],
+      modifierValue: 'maxweightrating>7.5',
+    },
+    comments: [],
+    catalogue: {
+      signCategory: 'condition_modifier',
+    },
+    image: {
+      sourceUrl:
+        'https://de.wikipedia.org/wiki/Datei:Zusatzzeichen_1053-33_-_Massenangabe_%E2%80%93_7,5_t_(420x231),_StVO_2017.svg',
+      licence: 'Public Domain',
+    },
+  },
+  {
+    osmValuePart: '1053-37',
+    signId: '1053-37',
+    name: 'Zusatzzeichen 1053-37',
+    descriptiveName: 'Massenangabe 12 t',
+    description:
+      '12 t zulässige Gesamtmasse einschließlich Anhänger überschreitet die angegebene Grenze',
+    kind: 'condition_modifier',
+    tagRecommendations: {
+      uniqueTags: [],
+      modifierValue: 'weight>12',
+    },
+    comments: [],
+    catalogue: {
+      signCategory: 'condition_modifier',
+    },
+    image: {
+      sourceUrl:
+        'https://de.wikipedia.org/wiki/Datei:Zusatzzeichen_1053-37_-_Massenangabe_%E2%80%93_12_t_(420x231),_StVO_2005.svg',
+      licence: 'Public Domain',
+    },
+  },
+  {
+    osmValuePart: '1060-33',
+    signId: '1060-33',
+    name: 'Zusatzzeichen 1060-33',
+    descriptiveName: 'Massenangabe 2,8 t',
+    description:
+      '2,8 t zulässige Gesamtmasse einschließlich Anhänger überschreitet die angegebene Grenze',
+    kind: 'condition_modifier',
+    tagRecommendations: {
+      uniqueTags: [],
+      modifierValue: 'weight>2.8',
+    },
+    comments: [
+      {
+        comment: 'Nur in Verbindung mit Zeichen 277 "Überholverbot für Kraftfahrzeuge über 3,5 t"',
+      },
+    ],
+    catalogue: {
+      signCategory: 'condition_modifier',
+    },
+    image: {
+      sourceUrl:
+        'https://de.wikipedia.org/wiki/Datei:Zusatzzeichen_1060-33_-_Massenangabe_-_2,8_t,_StVO_2017.svg',
       licence: 'Public Domain',
     },
   },
