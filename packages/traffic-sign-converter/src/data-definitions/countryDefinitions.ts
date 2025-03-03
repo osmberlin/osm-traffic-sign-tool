517b2f901fa6890dc16fdda94db07a7158c80c85
Tobias <tobias@fixmycity.de>
2025-03-03 16:27:47 +0100

    Package: Remove unused dependencies

--- packages/traffic-sign-converter/src/data-definitions/countryDefinitions.ts
+++ packages/traffic-sign-converter/src/data-definitions/countryDefinitions.ts
@@ -1,4 +1,3 @@
-import { z } from 'zod'
 import { trafficSignDataDE } from './DE/trafficSignDataDE.js'
 import type { SignType } from './TrafficSignDataTypes.js'
 
@@ -10,8 +9,6 @@ export const countryDefinitions = {
 export type CountryPrefixType = keyof typeof countryDefinitions
 
 export const countries = Object.keys(countryDefinitions) as [CountryPrefixType]
-
-export const CountryPrefixSchema = z.enum(countries)
 
 export const countryDefinitionMap = new Map<CountryPrefixType, SignType[]>(
   Object.entries(countryDefinitions) as [CountryPrefixType, SignType[]][],
